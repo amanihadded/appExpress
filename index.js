56d7ae886b1e94b065810f1225c4b48e7fa42179
amanihadded <amanihaddad850@gmail.com>
2024-09-17 15:30:39 +0100

    creer repo public css et mettre code auto refresh

--- index.js
+++ index.js
@@ -5,6 +5,26 @@ const port = 3002;
 app.use(express.urlencoded({ extended: true }));
 const Article = require("./models/myDataShema")
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+
+//auto refresh 
+//npm run watch
+const path = require("path");
+const livereload = require("livereload");
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, 'public'));
+ 
+ 
+const connectLivereload = require("connect-livereload");
+app.use(connectLivereload());
+ 
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
 
 // Route pour la page d'accueil
 app.get('/', (req, res) => {
