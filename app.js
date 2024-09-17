805b01960faf9131f908c7780c80a352d568b5b2
amanihadded <amanihaddad850@gmail.com>
2024-09-17 14:29:31 +0100

    post

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const port = 3002
+const port = 3002;
+app.use(express.urlencoded({ extended: true }));
+const Article = require("./models/myDataShema")
 
 app.get('/hello', (req, res) => {
     res.send('hello amani!');
@@ -20,3 +22,15 @@ mongoose
     });
 })
 .catch((err)=>{console.log(err)});
+
+
+app.post('/', (req,res)=> {
+    console.log(req.body);
+    const article = new Article(req.body);
+    article.save().then(()=>{
+        res.redirect("/");
+    }).catch((err)=>{
+        console.log(err);
+    });
+    
+});
