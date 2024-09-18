9a225a7ef94bb9a5d6b2576a5127c778750fc220
amanihadded <amanihaddad850@gmail.com>
2024-09-18 20:44:02 +0100

    post un employer

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const app = express();
 const port = 3002;
 app.use(express.urlencoded({ extended: true }));
-const Article = require("./models/myDataShema")
+const Custumer = require("./models/custumerShema")
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
@@ -41,7 +41,7 @@ liveReloadServer.server.once("connection", () => {
         
 // });
 
-
+//get request
 app.get('/user/add.html', (req, res) => {
     res.render("user/add");
 });
@@ -60,6 +60,25 @@ app.get('/', (req, res) => {
 });
 
 
+
+
+//post request
+app.post('/user/add.html', (req,res)=> {
+    const custumer = new Custumer(req.body);
+
+    custumer
+    .save().
+    then(()=>{
+        res.redirect("/user/add.html");
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+    
+    
+});
+
+//connexion avec base 
 mongoose
 .connect("mongodb+srv://haddadamani222:i6MmbtLF9lFP9b67@cluster0.bu82k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 .then(()=>{
@@ -70,13 +89,3 @@ mongoose
 .catch((err)=>{console.log(err)});
 
 
-app.post('/', (req,res)=> {
-    console.log(req.body);
-    const article = new Article(req.body);
-    article.save().then(()=>{
-        res.redirect("/");
-    }).catch((err)=>{
-        console.log(err);
-    });
-    
-});
